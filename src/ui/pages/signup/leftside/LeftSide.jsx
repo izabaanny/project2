f18c3368a5601b6570aa8de24616cfc277181e79
Yannick M <85898240+mugishayannick@users.noreply.github.com>
2022-09-09 10:14:30 +0200

    Add files via upload

---
+++ src/ui/pages/signup/leftside/LeftSide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LeftSide.css'
+import image from '../../../resources/images/library.png'
+
+function LeftSide() {
+  return (
+    <div className="left-container">
+      <img src={image} alt="" />
+    </div>
+  )
+}
+
+export default LeftSide
